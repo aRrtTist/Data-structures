5baaa4058c3c84dd966b859dc2e888ea72839c9c
Tian Ruan <43052152+aRrtTist@users.noreply.github.com>
2019-02-25 19:56:10 +0800

    Add files via upload

---
+++ SegmentTree/SegmentTree.java
@@ -0,0 +1,107 @@
+public class SegmentTree<E>{
+
+    private E[] data;
+    private E[] tree;
+    private Merger<E> merger;
+
+    public SegmentTree(E[] arr, Merger<E> merger){
+        this.merger = merger;
+        data = (E[])new Object[arr.length];
+        for(int i = 0; i < data.length; i++)
+            data[i] = arr[i];
+        tree = (E[])new Object[4 * arr.length];
+        buildSegmentTree(0, 0, data.length- 1);
+    }
+
+    private void buildSegmentTree(int treeIndex, int l, int r){
+        if(l == r){
+            tree[treeIndex] = data[l];
+            return;
+        }
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
+        int mid = l + (r - l) / 2;
+        buildSegmentTree(leftTreeIndex, l, mid);
+        buildSegmentTree(rightTreeIndex, mid + 1, r);
+        tree[treeIndex] = merger.merge(tree[leftTreeIndex], tree[rightTreeIndex]);
+    }
+
+    public E query(int queryL, int queryR){
+        if(queryL < 0 || queryL >= data.length || queryR < 0 || queryR >= data.length ||queryL > queryR)
+            throw new IllegalArgumentException("Index is illegal.");
+        return query(0, 0, data.length - 1, queryL, queryR);
+    }
+
+    private E query(int treeIndex, int l, int r, int queryL, int queryR){
+        if(queryL == l && queryR == r)
+            return tree[treeIndex];
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
+        int mid = l + (r - l) / 2;
+        if(queryR <= mid)
+            return query(leftTreeIndex, l, mid, queryL, queryR);
+        else if(queryL >= mid + 1)
+            return query(rightTreeIndex, mid + 1, r, queryL, queryR);
+        else {
+            E leftResult = query(leftTreeIndex, l, mid, queryL, mid);
+            E rightResult = query(rightTreeIndex, mid + 1, r, mid + 1, queryR);
+            return merger.merge(leftResult, rightResult);
+        }
+
+    }
+
+    public void set(int index, E e){
+        if(index < 0 || index >= data.length)
+            throw new IllegalArgumentException("Index is illegal.");
+        data[index] = e;
+        set(0, 0, data.length - 1, index, e);
+    }
+
+    private void set(int treeIndex, int l, int r, int index, E e){
+        if(l == r) {
+            tree[treeIndex] = e;
+            return;
+        }
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
+        int mid = l + (r - l) / 2;
+        if(index <= mid)
+            set(leftTreeIndex, l, mid, index, e);
+        else
+            set(rightTreeIndex, mid + 1, r, index, e);
+        tree[treeIndex] = merger.merge(tree[leftTreeIndex], tree[rightTreeIndex]);
+    }
+
+    public int getSize(){
+        return data.length;
+    }
+
+    public E get(int index){
+        if(index < 0 || index >= data.length)
+            throw new IllegalArgumentException("Index is illegal.");
+        return data[index];
+    }
+
+    private int leftChild(int index){
+        return 2 * index + 1;
+    }
+    private int rightChild(int index){
+        return 2 * index + 2;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder res = new StringBuilder();
+        res.append("[");
+        for(int i = 0; i < tree.length; i++){
+            if(tree[i] != null)
+                res.append(tree[i]);
+            else
+                res.append("null");
+            if(i != tree.length - 1)
+                res.append(", ");
+        }
+        res.append("]");
+        return res.toString();
+    }
+}
