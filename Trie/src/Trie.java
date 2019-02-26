aeed5b75d34060ce58d83d923f1aeb81decb27d9
Tian Ruan <43052152+aRrtTist@users.noreply.github.com>
2019-02-26 14:15:40 +0800

    Add files via upload

---
+++ Trie/src/Trie.java
@@ -0,0 +1,67 @@
+import java.util.TreeMap;
+
+public class Trie {
+
+    private class Node {
+        boolean isWord;
+        TreeMap<Character, Node> next;
+
+        public Node(){
+            isWord = false;
+            next = new TreeMap<>();
+        }
+
+        public Node(boolean isWord){
+            this.isWord = isWord;
+            next = new TreeMap<>();
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public Trie(){
+        root = new Node();
+        size = 0;
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public void add(String word){
+        Node cur = root;
+        for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            if(cur.next.get(c) == null)
+                cur.next.put(c, new Node());
+            cur = cur.next.get(c);
+        }
+        if(!cur.isWord) {
+            cur.isWord = true;
+            size++;
+        }
+    }
+
+    public boolean contains(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null)
+                return false;
+            cur = cur.next.get(c);
+        }
+        return cur.isWord;
+    }
+
+    public boolean isPrefix(String prefix){
+        Node cur = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if (cur.next.get(c) == null)
+                return false;
+            cur = cur.next.get(c);
+        }
+        return true;
+    }
+}
